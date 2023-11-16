05dd26fdd19ad4d73442610f7a974ff66cb6e4d8
Alex McCausland <alex.mccausland@gmail.com>
2023-11-16 06:59:14 +0000

    Add percs-client integration and subscribe button

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Script from "next/script"
+
 import '../global.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  function handlePercsLoad() {
+    window.PERCS.init({
+      clientKey: "gjZs6GpK2-7uBQjUbFgaWkTadtoA8J37-aCQxjU8yBZ1u4HAgMnjNBuFt8TJ2Umvbtu9kNAZjN/JBA0HKelPsWY7",
+      apiHost: "https://stg-api.getpercs.com",
+    })
+    document.body.append(document.createElement("percs-modal"))
+  }
+
+  return <>
+    <Script src="https://assets.getpercs.com/sdk/percs.js" onLoad={handlePercsLoad} />
+    <Component {...pageProps} />
+  </>
 }
--- pages/index.js
+++ pages/index.js
@@ -3,64 +3,37 @@ import Button from '../components/Button'
 import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
 
-function throwError() {
-  console.log(
-    // The function body() is not defined
-    document.body()
-  )
-}
-
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
-
-  useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
-
-    return () => {
-      clearInterval(r)
-    }
-  }, [increment])
+  function handleSubscribe() {
+    console.info("subscribe")
+    window.PERCS.openModal({
+      subscribe: {
+        required: true,
+        headingImage: {
+          src: "https://assets.getpercs.com/logo.svg",
+        },
+        headingText: "Stars, stripes & savings!",
+        descriptionText: "Great deals sent straight to your inbox! Connect your wallet & subscribe to get notified about exclusive offers!",
+        successHeadingText: "Thanks for signing up!",
+        successDescriptionText: "Be on the lookout for new product drops and exclusive offers in your inbox!",
+        providers: ["xmtp_messaging"],
+      }
+    })
+  }
 
   return (
     <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
+      <h1>NextJS Demo</h1>
       <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
-        on edits made to your React components, without ever losing component
-        state.
+        Sample NextJS 13 app with integration for Percs SDK.
       </p>
       <hr className={styles.hr} />
       <div>
         <p>
-          Auto incrementing value. The counter won't reset after edits or if
-          there are errors.
+          Subscribe with your wallet
         </p>
-        <p>Current value: {count}</p>
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>Component with state.</p>
-        <ClickCount />
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          The button below will throw 2 errors. You'll see the error overlay to
-          let you know about the errors but it won't break the page or reset
-          your state.
-        </p>
-        <Button
-          onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
-          }}
-        >
-          Throw an Error
+        <Button onClick={handleSubscribe}>
+          Subscribe
         </Button>
       </div>
       <hr className={styles.hr} />
